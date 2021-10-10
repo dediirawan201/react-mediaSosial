ca7f744b32f60dd96d0b95bce8f7ea707ce721d8
dedi <dediirwansyah1925@gmail.com>
2021-10-10 07:42:34 +0700

    membuat component rightbar

--- src/components/moleculs/Rightbar/index.js
+++ src/components/moleculs/Rightbar/index.js
@@ -3,7 +3,128 @@ import './rightbar.css';
 const Rightbar = () => {
     return (
         <div className="rightbar">
-           rightbar 
+           <div className="rightbarWrapper">
+               <div className="birthdayContainer">
+                   <img className="birthdayImg" src="assets/gift.png" alt="" />
+                   <span className="birthdayText">
+                       <b>Pola Faster</b> and <b>3 other friends</b> have a birthday today.
+                   </span>
+               </div>
+               <img className="rightbarAd" src="assets/ad.png" alt="" />
+               <h4 className="rightbarTitle">Outline Friends</h4>
+               <ul className="rightbarFriendList">
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img 
+                           className="rightbarprofileImg"
+                           src="assets/person/3.jpeg"
+                           alt=""
+                           />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className="rightbarUserName">Dwi jonson</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img 
+                           className="rightbarprofileImg"
+                           src="assets/person/3.jpeg"
+                           alt=""
+                           />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className="rightbarUserName">Dwi jonson</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img 
+                           className="rightbarprofileImg"
+                           src="assets/person/3.jpeg"
+                           alt=""
+                           />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className="rightbarUserName">Dwi jonson</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img 
+                           className="rightbarprofileImg"
+                           src="assets/person/3.jpeg"
+                           alt=""
+                           />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className="rightbarUserName">Dwi jonson</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img 
+                           className="rightbarprofileImg"
+                           src="assets/person/3.jpeg"
+                           alt=""
+                           />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className="rightbarUserName">Dwi jonson</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img 
+                           className="rightbarprofileImg"
+                           src="assets/person/3.jpeg"
+                           alt=""
+                           />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className="rightbarUserName">Dwi jonson</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img 
+                           className="rightbarprofileImg"
+                           src="assets/person/3.jpeg"
+                           alt=""
+                           />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className="rightbarUserName">Dwi jonson</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img 
+                           className="rightbarprofileImg"
+                           src="assets/person/3.jpeg"
+                           alt=""
+                           />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className="rightbarUserName">Dwi jonson</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img 
+                           className="rightbarprofileImg"
+                           src="assets/person/3.jpeg"
+                           alt=""
+                           />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className="rightbarUserName">Dwi jonson</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img 
+                           className="rightbarprofileImg"
+                           src="assets/person/3.jpeg"
+                           alt=""
+                           />
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className="rightbarUserName">Dwi jonson</span>
+                   </li>
+               </ul>
+           </div>
         </div>
     )
 }
